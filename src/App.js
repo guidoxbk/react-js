69ca9d69ea15e2de2947cbc963b3589bc53007c8
guidoxbk <guidox_bk@hotmail.com>
2021-05-10 20:19:26 -0300

    paso proyecto a react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import logo from './logo.svg';
+
 import './App.css';
+var items=[
+    'uno' ,
+    'dos',
+    'tres',
+    'cuatro',
+    'cinco',
+    'seis',
+    'siete',
+    'ocho',
+    ]
+let logo =''
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          nuevo proyecto react js
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          prueba de cambios
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="app-header">
+        <div id="encabezado">
+
+                    <div className="nav-logo">
+                        <img src="img/Sin título.png"/>
+                    </div>  
+
+                    <div className="barra">
+                <input className="searchbar" type="search" placeholder="Qué producto buscás?"/>
+            </div>
+                    
+                    <div>
+                <a href="#"><img className="logo-carrito" src="img/shopping_cart_white_48dp.svg" alt="Carrito"/></a>
+            </div>
+        </div>
+        </div>
+        
+        
+            
+        <div id="artic">
+            <div><h2>accesorios</h2></div>
+            <div><h2>hogar y deco</h2></div>
+            <div><h2>tecnologia</h2></div>
+            <div><h2>promociones del mes</h2></div>
+        </div>
+        <div id="banner"> 
+            
+            <button><h3 className="Conoce-mas">conoce mas</h3></button>
+           
+        </div>
+
+            <div className="beneficios">
+                <div className="conteiner-beneficios">
+                    <a href="delivery"><img src="img/delivery.svg" alt="delivery"/></a>
+                    <div className="aside-box">
+                    <h3>Envíos a todo el país</h3>
+                    <p>Comprá desde la comodidad de tu casa</p>
+                    </div>
+                </div>
+                
+                <div className="conteiner-tarj-benef">
+            <a href="#compra-segura"><img src="img/shield.svg" alt="compra segura"/></a>
+            <div className="aside-box">
+                <h3>Seguridad</h3>
+                <p>Compras protegidas por Mercado Pago</p>
+            </div>
+        </div>
+
+
+            </div> 
+
+            
+            <div className="item">
+            {
+
+                items.map( function () {
+                    return <div className="item-product"> 
+                   <img src="/img/vinilos-deco1.jpg" alt="logo"/>
+                    <p><b className=" link-compra">comprar</b></p> 
+                     </div> 
+
+                })
+        
+          }</div>
+
+        <div className="ver-mas">
+                Ver Mas
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        
+<div className="footer">
+<p>
+                Direccion 444,piso 5. CABA
+            </p>
+            <p> 
+                tienda@dominio.com.ar
+            </p>
+            <p>
+                +54 9 299-5643132
+            </p>
+
+</div>
+
+        
+
+    </>
   );
 }
 
